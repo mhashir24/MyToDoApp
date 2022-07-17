61d71e5883b44e186415ac79d41fa6a5a0ead8d0
mhashir24 <hashir201514@gmail.com>
2022-07-17 15:56:45 +0500

    add console

--- src/Components/SwipeComponent/index.js
+++ src/Components/SwipeComponent/index.js
@@ -24,6 +24,7 @@ export default function SwipeButtonCustom({
   onEdit,
   toggleBottomNavigationView,
 }) {
+  console.log(index, 'index')
   const leftButton = (
     <SwipeButtonsContainer
       style={{
